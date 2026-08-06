25d1bf434df5414726e38c3518556e7c6df81894
andrenascsouza-espec <andrenascsouza@gmail.com>
2026-08-06 06:05:56 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION='gmv-v2515-logo-dourada-forte';
+const CACHE_VERSION='gmv-v270-menu-permissoes';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys(); await Promise.all(keys.map(k=>caches.delete(k))); await self.clients.claim();})());});
 self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>fetch(event.request)));});
