cef3f0f4fcb6f22d0c9979e671dfcd56125d36eb
andrenascsouza-espec <andrenascsouza@gmail.com>
2026-08-06 07:14:34 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION='gmv-v270-menu-permissoes';
+const CACHE_VERSION='gmv-v27-2menu-permissoes';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil((async()=>{const keys=await caches.keys(); await Promise.all(keys.map(k=>caches.delete(k))); await self.clients.claim();})());});
 self.addEventListener('fetch',event=>{event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>fetch(event.request)));});
